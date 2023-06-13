6a90f2790521dfa3f93848d848e3946c0d708e16
谭鑫森 <849610435@qq.com>
2023-06-13 11:32:35 +0800

    插槽

--- src/views/slot/Slot2.vue
+++ src/views/slot/Slot2.vue
@@ -2,12 +2,14 @@
     <h3>Slot2</h3>
     <!-- <slot></slot> -->
     <ul>
-        <li v-for="item in props.arr" :key="item.id">{{ item.name }}</li>
+        <li v-for="(item, index) in props.arr" :key="item.id">
+            <!-- 作用域插槽：可以将数据回传给父组件 -->
+            <slot :$row="item" :$index="index"></slot>
+        </li>
     </ul>
 </template>
 <script setup lang='ts'>
 const props = defineProps(["arr"])
-console.log(props.arr);
 
 </script>
 <style lang='scss' scoped>
--- src/views/slot/SlotText.vue
+++ src/views/slot/SlotText.vue
@@ -8,14 +8,22 @@
                 <div>原来如此</div>
             </template>
         </Slot1>
-        <Slot2 :arr="arr"></Slot2>
+        <Slot2 :arr="arr">
+            <template v-slot="{ $row, $index }">
+                <h3>{{ $row.name }}---{{ $index }}</h3>
+            </template>
+        </Slot2>
     </div>
 </template>
 <script setup lang='ts'>
 import Slot1 from "./Slot1.vue"
 import Slot2 from "./Slot2.vue"
 import { ref } from "vue"
+/* 
+    作用域插槽：就是可以传递数据的插槽，子组件可以将数据回传给父组件，父组件可以决定这些回传的
+    数据是以何种数据结构或者外观在子组件内部去展示
 
+*/
 const arr = ref([
     {
         id: 1,
